3a62f76bc7e1373cb22c4d33166677d14f150c50
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-11-12 21:15:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code solution here
+class President {
+  constructor(name, poiticalParty, yearsInOffice, homeState) {
+    this.name = name;
+    this.politicalParty = politicalParty;
+    this.yearsInOffice = yearsInOffice;
+    this.homeState = homeState;
+  }
+  
+  veto() {
+    return("NO!");
+  }
+  
+  passBill() {
+    return("You can do that!");
+  }
+  
+  doCharity() {
+    return("I like to help People.")
+  }
+  
+  conductPressInterview() {
+    return("I am proud to be an American.")
+  }
+}
